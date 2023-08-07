1798a06a4b499f658ff6738eb1be25ed38853202
elif76nisa <elifozentelek@gmail.com>
2023-08-07 18:37:55 +0200

    task 1

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -4,7 +4,8 @@ public class Merhaba {
     public static void main(String[] args) {
         System.out.println("merhaba 1");
 
-        System.out.println("merhaba 2);
+        System.out.println("merhaba 2");
+
 
 
     }
